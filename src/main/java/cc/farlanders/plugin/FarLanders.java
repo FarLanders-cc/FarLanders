a0b8150310c637a9de254c07332207b53d105f39
clxrityy <clxrityxyz@gmail.com>
2025-06-15 01:43:54 -0400

    add command

--- src/main/java/cc/farlanders/plugin/FarLanders.java
+++ src/main/java/cc/farlanders/plugin/FarLanders.java
@@ -1,19 +1,26 @@
 package cc.farlanders.plugin;
 
-import java.util.logging.Logger;
-
 import org.bukkit.plugin.java.JavaPlugin;
+
+import cc.farlanders.command.CommandHandler;
+import cc.farlanders.command.cmds.GenerateFarLandsCommand;
 
 public final class FarLanders extends JavaPlugin {
 
-    @Override
-    public Logger getLogger() {
-        return super.getLogger();
-    }
+    private final CommandHandler handler = new CommandHandler();
 
     @Override
     public void onEnable() {
         getLogger().info("FarLanders enabled!");
+
+        var farlandersCommand = getCommand("farlanders");
+        if (farlandersCommand != null) {
+            farlandersCommand.setExecutor(handler);
+            handler.register(new GenerateFarLandsCommand());
+            getLogger().info("FarLands command registered successfully!");
+        } else {
+            getLogger().severe("Failed to register 'farlanders' command: command not found in plugin.yml!");
+        }
     }
 
     @Override
